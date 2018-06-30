fe18f545681e007a8d18f2fb6d1ebbe3aa5a4695
Herr Zatacke <zatacke@gmail.com>
2018-06-30 15:42:07 +0200

    correctly read positions from image

--- src/writer/getPixels.js
+++ src/writer/getPixels.js
@@ -7,22 +7,22 @@ const getPixels = (bitmap, brightness = 1) => {
 
   const pixels = [];
   const { width, height } = bitmap;
-  const pixelData = ndarray(bitmap.data, [width, height, 4]);
+  const pixelData = ndarray(bitmap.data, [height, width, 4]);
 
   for (let x = 0; x < width; x++) {
     const col = [];
     pixels.push(col);
     for (let y = 0; y < height; y++) {
 
-      let r = pixelData.get(x, y, 0);
-      let g = pixelData.get(x, y, 1);
-      let b = pixelData.get(x, y, 2);
+      let r = pixelData.get(y, x, 0);
+      let g = pixelData.get(y, x, 1);
+      let b = pixelData.get(y, x, 2);
 
       r = parseInt(r * brightness, 10);
       g = parseInt(g * brightness, 10);
       b = parseInt(b * brightness, 10);
 
-      col.push(rgb2int(r, g, b));
+      col.unshift(rgb2int(r, g, b));
     }
   }
   return pixels;
