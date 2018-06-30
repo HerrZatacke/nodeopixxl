866515a3e3f893d28c6124d9a7ad061037c8295b
Herr Zatacke <zatacke@gmail.com>
2018-06-30 20:30:24 +0200

    eliminate ipc communication - run everything in one process

--- src/writer/index.js
+++ src/writer/index.js
@@ -1,53 +1,104 @@
-const ipc = require('node-ipc');
-const ipcConfig = require('./ipcConfig');
+const fs = require('fs');
+const ws281x = require('rpi-ws281x-native');
+const Jimp = require('jimp');
+const getPixels = require('./getPixels');
 
-Object.assign(ipc.config, ipcConfig);
+const NUM_LEDS = 160;
 
 class Writer {
+
   constructor() {
-    this.socket = null;
+    this.renderTimeout = null;
+    this.loadTimeout = null;
+    this.fps = 30;
+    this.pixels = [[]];
+    this.offset = 0;
+    this.canAcceptNewImage = true;
   }
 
   init() {
-    ipc.connectToNet('nodeopixxl', () => {
-      this.socket = ipc.of.nodeopixxl;
-      this.addIPCBindings();
+    ws281x.init(NUM_LEDS);
+
+// trap the SIGINT and reset before exit
+    process.on('SIGINT', () => {
+      ws281x.reset();
+      process.nextTick(() => {
+        process.exit(0);
+      });
     });
   }
 
-  addIPCBindings() {
-    this.socket.on('connect', () => {
-      console.log('## connected ##');
-      this.socket.emit('nodeopixxl-imagedata', [[]]);
-    });
+  setImageFile(imagePath) {
+    this.stopAnimation();
+    if (!this.canAcceptNewImage) {
+      console.log('busy loading');
+      return;
+    }
 
-    this.socket.on('disconnect', () => {
-      console.log('i miss da serverz');
-    });
+    console.log(`loading file ${imagePath}`);
+    this.canAcceptNewImage = false;
 
-    this.socket.on('nodeopixxl-wrap', () => {
-      console.log('wrap!');
-    });
-    // too much?
-    // this.socket.on('nodeopixxl-index', data => {
-    //   console.log(`@${data}`);
-    // });
-  }
-
-  setImageFile(imagepath) {
-    this.socket.emit('nodeopixxl-imagefile', imagepath);
+    // read a file after a second
+    global.clearTimeout(this.loadTimeout);
+    this.loadTimeout = global.setTimeout(() => {
+      Jimp.read(imagePath)
+        .then((image) => {
+          this.pixels = getPixels(image.bitmap);
+          console.log(`received pixels ${this.pixels.length}x${this.pixels[0].length}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+        .then((image) => {
+          fs.unlinkSync(imagePath);
+          this.canAcceptNewImage = true;
+        });
+    }, 1000);
   }
 
   start() {
-    this.socket.emit('nodeopixxl-start');
+    console.log('start');
+    this.startAnimation();
   }
 
   stop() {
-    this.socket.emit('nodeopixxl-stop');
+    console.log('stop');
+    this.stopAnimation();
   }
 
   setFPS(fps = 30) {
-    this.socket.emit('nodeopixxl-fps', fps);
+    console.log(`fps: ${fps}`);
+    this.fps = fps;
+  }
+
+
+  stopAnimation() {
+    global.clearTimeout(this.renderTimeout);
+    this.renderTimeout = null;
+    this.offset = 0;
+    ws281x.render(new Uint32Array(NUM_LEDS));
+  }
+
+  startAnimation() {
+    global.clearTimeout(this.renderTimeout);
+    if (this.pixels.length < 2) {
+      this.stopAnimation();
+      return;
+    }
+    this.renderTimeout = global.setTimeout(() => {
+
+      // process.stdout.write(`offset:${this.offset}  width:${this.pixels.length}  fps:${this.fps}\r`);
+      console.log(`offset:${this.offset}  width:${this.pixels.length}  fps:${this.fps}`);
+
+      ws281x.render(new Uint32Array(this.pixels[this.offset]));
+
+      this.offset = (this.offset + 1) % this.pixels.length;
+
+      if (this.offset !== 0) {
+        this.startAnimation();
+      }
+
+    }, 1000 / this.fps);
   }
 }
 
