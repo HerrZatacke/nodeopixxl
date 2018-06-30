0926f1a9fdceef5ce2bde590d84296e48ef15c95
Herr Zatacke <zatacke@gmail.com>
2018-06-30 17:27:54 +0200

    trying to find cause of freezes made service a class

--- src/writer/service.js
+++ src/writer/service.js
@@ -18,77 +18,97 @@ process.on('SIGINT', () => {
 });
 
 
-let fps = 30;
-let pixels = [[]];
-let offset = 0;
-let renderInterval = null;
-
 Object.assign(ipc.config, ipcConfig);
 
-ipc.serveNet(() => {
-  ipc.server.on('nodeopixxl-imagefile', (imagePath) => {
-    stopAnimation();
+class Service {
+  constructor() {
+    this.socket = null;
+    this.renderTimeout = null;
+    this.fps = 30;
+    this.pixels = [[]];
+    this.offset = 0;
+  }
 
-    Jimp.read(imagePath)
-      .then((image) => {
-        pixels = getPixels(image.bitmap);
-        console.log(`received pixels ${pixels.length}x${pixels[0].length}`);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      })
-      .then((image) => {
-        fs.unlinkSync(imagePath);
+  start() {
+    ipc.serveNet(() => {
+
+      ipc.server.on('nodeopixxl-imagefile', (imagePath) => {
+        this.stopAnimation();
+
+        // read a file after a 2 frame delay
+        setTimeout(() => {
+          Jimp.read(imagePath)
+            .then((image) => {
+              this.pixels = getPixels(image.bitmap);
+              console.log(`received pixels ${this.pixels.length}x${this.pixels[0].length}`);
+            })
+            .catch((err) => {
+              console.log(err.message);
+            })
+            .then((image) => {
+              fs.unlinkSync(imagePath);
+            });
+        }, 500 / this.fps);
+
       });
-  });
 
-  ipc.server.on('nodeopixxl-fps', (data, socket) => {
-    console.log(`newfps: ${data}`);
-    fps = data;
-    if (renderInterval) {
-      startAnimation(socket);
+      ipc.server.on('nodeopixxl-fps', (fps, socket) => {
+        console.log(`fps: ${fps}`);
+        this.fps = fps;
+      });
+
+      ipc.server.on('nodeopixxl-start', (data, socket) => {
+        console.log('start');
+        this.startAnimation(socket);
+      });
+
+      ipc.server.on('nodeopixxl-stop', () => {
+        console.log('stop');
+        this.stopAnimation();
+      });
+
+      ipc.server.on('socket.disconnected', () => {
+        console.log('socket.disconnected');
+        this.stopAnimation();
+      });
+
+      ipc.server.on('connect', () => {
+        console.log('connect');
+      });
+    });
+    ipc.server.start();
+  }
+
+  stopAnimation() {
+    global.clearTimeout(this.renderTimeout);
+    this.renderTimeout = null;
+    this.offset = 0;
+    process.stdout.write('\r\n');
+    ws281x.render(new Uint32Array(NUM_LEDS));
+  }
+
+  startAnimation(socket) {
+    global.clearTimeout(this.renderTimeout);
+    if (this.pixels.length < 2) {
+      this.stopAnimation();
+      return;
     }
-  });
+    this.renderTimeout = global.setTimeout(() => {
 
-  ipc.server.on('nodeopixxl-start', (data, socket) => {
-    console.log('start!!!');
-    startAnimation(socket);
-  });
+      process.stdout.write(`offset:${this.offset}  width:${this.pixels.length}  fps:${this.fps}\r`);
+      ws281x.render(new Uint32Array(this.pixels[this.offset]));
 
-  ipc.server.on('nodeopixxl-stop', () => {
-    console.log('stahpp!!');
-    stopAnimation();
-  });
+      this.offset = (this.offset + 1) % this.pixels.length;
 
-  ipc.server.on('socket.disconnected', () => {
-    console.log('disco!');
-    stopAnimation();
-  });
-});
+      if (this.offset !== 0) {
+        this.startAnimation();
+      }
 
-const stopAnimation = () => {
-  global.clearInterval(renderInterval);
-  renderInterval = null;
-  offset = 0;
-  ws281x.render(new Uint32Array(NUM_LEDS));
-};
+    }, 1000 / this.fps);
+  }
+}
 
-const startAnimation = (socket) => {
-  global.clearInterval(renderInterval);
-  renderInterval = global.setInterval(() => {
 
-    offset = (offset + 1) % pixels.length;
 
-    if (offset === 0) {
-      ipc.server.emit(socket, 'nodeopixxl-wrap');
-    }
-
-    // too much?
-    // ipc.server.emit(socket, 'nodeopixxl-index', offset);
-
-    ws281x.render(new Uint32Array(pixels[offset]));
-
-  }, 1000 / fps);
-};
-
-ipc.server.start();
+const service = new Service();
+service.start();
