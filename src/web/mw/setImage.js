76680d6c66a808343e43e53519c8ad4cbccb35e2
Herr Zatacke <zatacke@gmail.com>
2018-07-07 23:39:30 +0200

    really?

--- src/web/mw/setImage.js
+++ src/web/mw/setImage.js
@@ -2,11 +2,17 @@ const path = require('path');
 const writer = require('../../writer');
 
 const mwSetImage = (req, res) => {
-  const imageData = new Uint8ClampedArray(JSON.parse(req.body.image));
-  writer.setImageFile(imageData);
-  res.json({
-    length: imageData.length
-  });
+  if (!writer.isRunning) {
+    const imageData = new Uint8ClampedArray(JSON.parse(req.body.image));
+    writer.setImageFile(imageData);
+    res.json({
+      length: imageData.length
+    });
+  } else {
+    res.json({
+      already: 'running',
+    });
+  }
 };
 
 module.exports = mwSetImage;
