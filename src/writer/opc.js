b4d52f3a0cc54e3d7a0c72b44ebed6457bc04729
Herr Zatacke <zatacke@gmail.com>
2018-07-16 22:22:05 +0200

    refactored

--- src/writer/opc.js
+++ src/writer/opc.js
@@ -5,69 +5,64 @@
  * This file is released into the public domain.
  */
 
-var net = require('net');
+const net = require('net');
 
-var OPC = function(host, port)
-{
-    this.host = host;
-    this.port = port;
-    this.pixelBuffer = null;
+const OPC = function(host, port) {
+  this.host = host;
+  this.port = port;
+  this.pixelBuffer = null;
 };
 
-OPC.prototype._reconnect = function()
-{
-    var _this = this;
+OPC.prototype._reconnect = function() {
+  const _this = this;
 
-    this.socket = new net.Socket()
-    this.connected = false;
+  this.socket = new net.Socket();
+  this.connected = false;
 
-    this.socket.onclose = function() {
-        console.log("Connection closed");
-        _this.socket = null;
-        _this.connected = false;
-    }
+  this.socket.onclose = function() {
+    console.log("Connection closed");
+    _this.socket = null;
+    _this.connected = false;
+  };
 
-    this.socket.connect(this.port, this.host, function() {
-        console.log("Connected to " + _this.socket.remoteAddress);
-        _this.connected = true;
-        _this.socket.setNoDelay();
-    });
-}
+  this.socket.connect(this.port, this.host, function() {
+    console.log("Connected to " + _this.socket.remoteAddress);
+    _this.connected = true;
+    _this.socket.setNoDelay();
+  });
+};
 
-OPC.prototype.writePixels = function()
-{
-    if (!this.socket) {
-        this._reconnect();
-    }
-    if (!this.connected) {
-        return;
-    }
-    this.socket.write(this.pixelBuffer);
-}
+OPC.prototype.writePixels = function() {
+  if (!this.socket) {
+    this._reconnect();
+  }
+  if (!this.connected) {
+    return;
+  }
+  this.socket.write(this.pixelBuffer);
+};
 
-OPC.prototype.setPixelCount = function(num)
-{
-    var length = 4 + num*3;
-    if (this.pixelBuffer == null || this.pixelBuffer.length != length) {
-        this.pixelBuffer = new Buffer(length);
-    }
+OPC.prototype.setPixelCount = function(num) {
+  const length = 4 + num*3;
+  if (this.pixelBuffer == null || this.pixelBuffer.length !== length) {
+    this.pixelBuffer = new Buffer(length);
+  }
 
-    // Initialize OPC header
-    this.pixelBuffer.writeUInt8(0, 0);           // Channel
-    this.pixelBuffer.writeUInt8(0, 1);           // Command
-    this.pixelBuffer.writeUInt16BE(num * 3, 2);  // Length
-}
+  // Initialize OPC header
+  this.pixelBuffer.writeUInt8(0, 0);           // Channel
+  this.pixelBuffer.writeUInt8(0, 1);           // Command
+  this.pixelBuffer.writeUInt16BE(num * 3, 2);  // Length
+};
 
-OPC.prototype.setPixel = function(num, r, g, b)
-{
-    var offset = 4 + num*3;
-    if (this.pixelBuffer == null || offset + 3 > this.pixelBuffer.length) {
-        this.setPixelCount(num + 1);
-    }
+OPC.prototype.setPixel = function(num, r, g, b) {
+  const offset = 4 + num * 3;
+  if (this.pixelBuffer == null || offset + 3 > this.pixelBuffer.length) {
+    this.setPixelCount(num + 1);
+  }
 
-    this.pixelBuffer.writeUInt8(Math.max(0, Math.min(255, r | 0)), offset);
-    this.pixelBuffer.writeUInt8(Math.max(0, Math.min(255, g | 0)), offset + 1);
-    this.pixelBuffer.writeUInt8(Math.max(0, Math.min(255, b | 0)), offset + 2);
-}
+  this.pixelBuffer.writeUInt8(Math.max(0, Math.min(255, r | 0)), offset);
+  this.pixelBuffer.writeUInt8(Math.max(0, Math.min(255, g | 0)), offset + 1);
+  this.pixelBuffer.writeUInt8(Math.max(0, Math.min(255, b | 0)), offset + 2);
+};
 
 module.exports = OPC;
