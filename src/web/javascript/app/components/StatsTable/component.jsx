e414c27936a5f6a65a979e7d80cca83fd7f29f64
Herr Zatacke <zatacke@gmail.com>
2018-07-19 01:59:20 +0200

    proptypes and table corrected

--- src/web/javascript/app/components/StatsTable/component.jsx
+++ src/web/javascript/app/components/StatsTable/component.jsx
@@ -3,34 +3,36 @@ import PropTypes from 'prop-types';
 
 const StatsTable = props => (
   <table className="stats-table">
-    <tr>
-      <th>single frame duration</th>
-      <td>{props.timeout}</td>
-    </tr>
-    <tr>
-      <th>real FPS</th>
-      <td>{props.realFps}</td>
-    </tr>
-    <tr>
-      <th>complete duration</th>
-      <td>{props.duration}</td>
-    </tr>
-    <tr>
-      <th>physical dimensions</th>
-      <td>{`${props.imageWidth} ✕ ${props.imageHeight}`}</td>
-    </tr>
-    <tr>
-      <th>pixel dimensions</th>
-      <td>{`${props.width}px ✕ ${props.height}px`}</td>
-    </tr>
-    <tr>
-      <th>m/s</th>
-      <td>{props.mps}</td>
-    </tr>
-    <tr>
-      <th>km/h</th>
-      <td>{props.kph}</td>
-    </tr>
+    <tbody>
+      <tr>
+        <th>single frame duration</th>
+        <td>{props.timeout}</td>
+      </tr>
+      <tr>
+        <th>real FPS</th>
+        <td>{props.realFps}</td>
+      </tr>
+      <tr>
+        <th>complete duration</th>
+        <td>{props.duration}</td>
+      </tr>
+      <tr>
+        <th>physical dimensions</th>
+        <td>{`${props.imageWidth} ✕ ${props.imageHeight}`}</td>
+      </tr>
+      <tr>
+        <th>pixel dimensions</th>
+        <td>{`${props.width}px ✕ ${props.height}px`}</td>
+      </tr>
+      <tr>
+        <th>m/s</th>
+        <td>{props.mps}</td>
+      </tr>
+      <tr>
+        <th>km/h</th>
+        <td>{props.kph}</td>
+      </tr>
+    </tbody>
   </table>
 );
 
@@ -40,10 +42,10 @@ StatsTable.propTypes = {
   timeout: PropTypes.string.isRequired,
   mps: PropTypes.string.isRequired,
   kph: PropTypes.string.isRequired,
-  width: PropTypes.string.isRequired,
-  height: PropTypes.string.isRequired,
   imageWidth: PropTypes.string.isRequired,
   imageHeight: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
 };
 
 StatsTable.defaultProps = {
