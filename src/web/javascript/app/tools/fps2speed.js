52f5a69535b47e53b0cb86ea9fa0d8f53de7464f
Herr Zatacke <zatacke@gmail.com>
2018-07-18 17:50:04 +0200

    added "real" fps to calculation

--- src/web/javascript/app/tools/fps2speed.js
+++ src/web/javascript/app/tools/fps2speed.js
@@ -3,23 +3,24 @@ const pixelsPerMeter = 1000 / pixelSize;
 const precision = 2;
 
 const fps2speed = (fps, width, height) => {
-
-  const metersPerSecond = fps / pixelsPerMeter;
+  const realFps = 1000 / Math.floor(1000 / fps);
+  const metersPerSecond = realFps / pixelsPerMeter;
   const kmPerHour = metersPerSecond * 3.6;
   const imageWidth = width / pixelsPerMeter;
   const imageHeight = height / pixelsPerMeter;
   const duration = imageWidth / metersPerSecond;
+  const timeout = Math.floor(1000 / realFps);
 
   return {
+    realFps: `${realFps.toFixed(precision)} fps`,
     base: `${pixelsPerMeter.toFixed(precision)} px/m`,
     pixelSize: `${pixelSize.toFixed(precision)} mm`,
     imageWidth: `${imageWidth.toFixed(precision)} m`,
     imageHeight: `${imageHeight.toFixed(precision)} m`,
     duration: `${duration.toFixed(precision)} s`,
-    speeds: [
-      `${metersPerSecond.toFixed(precision)} m/s`,
-      `${kmPerHour.toFixed(precision)}' km/h`,
-    ],
+    timeout: `${timeout} ms`,
+    mps: `${metersPerSecond.toFixed(precision)} m/s`,
+    kph: `${kmPerHour.toFixed(precision)}' km/h`,
   };
 
 };
