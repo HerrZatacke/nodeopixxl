b285d8795706c4d6df45e4a0feb23dd599fa4720
Herr Zatacke <zatacke@gmail.com>
2018-07-18 12:02:38 +0200

    clean unused/migrated from old.js and index.html

--- src/web/javascript/old.js
+++ src/web/javascript/old.js
@@ -1,76 +1,11 @@
 /* eslint-disable */
 import '../scss/old.scss';
 
-const fileInput = document.querySelector('input[type=file]');
-const canvas = document.querySelector('canvas');
 const startButton = document.querySelector('#start-image');
 const stopButton = document.querySelector('#stop-image');
 const setFPSButton = document.querySelector('#set-fps');
 const fpsInput = document.querySelector('#fps');
-const img = new Image();
-const ctx = canvas.getContext('2d');
-
-canvas.style.display = 'none';
-
-// const getScaledImageBlob = () => {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       // const filename = fileInput.files[0].name.toLowerCase().replace(/[\W]*/gi, '');
-//       if (canvas.toBlob) {
-//         canvas.toBlob(blob => resolve(blob), 'image/png');
-//       } else {
-//         resolve(canvas.msToBlob());
-//       }
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// };
-
-const getScaledImageData = () => {
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  return [...imageData.data]
-    // .map((value, index) => (
-    //   String.fromCharCode(value)
-    // ))
-    .filter((val, index) => (
-      (index + 1) % 4 !== 0
-    ));
-};
-
-fileInput.addEventListener('change', (ev) => {
-  ev.preventDefault();
-
-  ctx.fillRect(0,0, canvas.width, canvas.height);
-
-  const reader = new FileReader();
-
-  reader.onload = (ev) => {
-    if(ev.target.readyState === FileReader.DONE) {
-      img.onload = () => {
-        canvas.width = img.naturalWidth / img.naturalHeight * 160;
-        canvas.height = 160;
-
-        canvas.style.width = `${canvas.width}px`;
-        canvas.style.height = `${canvas.height}px`;
-
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-        const pixels = getScaledImageData();
-
-        exampleSocket.send(JSON.stringify({
-          setImage: pixels,
-        }));
-
-      };
-      img.src = ev.target.result;
-    }
-  };
-
-  if (fileInput.files && fileInput.files[0]) {
-    reader.readAsDataURL(fileInput.files[0]);
-  }
-});
+const exampleSocket = new WebSocket(`ws://${window.location.hostname}:3001/`);
 
 startButton.addEventListener('click', () => {
   exampleSocket.send(JSON.stringify({
@@ -89,9 +24,3 @@ setFPSButton.addEventListener('click', () => {
     fps: fpsInput.value,
   }));
 });
-
-const exampleSocket = new WebSocket(`ws://${window.location.hostname}:3001/`);
-
-exampleSocket.onmessage = (event) => {
-  // console.log(event.data);
-};
