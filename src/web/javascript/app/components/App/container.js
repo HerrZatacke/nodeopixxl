e9b94422c3695f4942806a54617640f26b0c8071
Herr Zatacke <zatacke@gmail.com>
2018-07-18 23:17:23 +0200

    simpler stats display

--- src/web/javascript/app/components/App/container.js
+++ src/web/javascript/app/components/App/container.js
@@ -1,15 +1,9 @@
 import { connect } from 'react-redux';
 import fps2speed from '../../tools/fps2speed';
 
-const mapStateToProps = state => ({
-  fps: state.fps,
-  offset: state.offset,
-  width: state.image.width,
-  height: state.image.height,
-  serverBusy: state.serverBusy,
-  animationRunning: state.animationRunning,
-  speed: fps2speed(state.fps, state.image.width, state.image.height),
-});
+const mapStateToProps = state => (
+  fps2speed(state.fps, state.image.width, state.image.height)
+);
 
 const mapDispatchToProps = (/* dispatch */) => ({
 });
